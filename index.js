18520f930ea0fa796797336a7cb696376b2c6c49
Aleph-Phoebe <phbwangui47@gmail.com>
2024-06-29 16:07:11 +0300

    COMPLETED

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = (a, b) => {
+    if (b === 0) {
+      Error('Division by zero is not allowed');
+    }
+    return a / b;
+  };
+
+const square = (x) => {
+    return x * x;
+  };
+
+const add = (a, b) => {
+    return a + b;
+  };
